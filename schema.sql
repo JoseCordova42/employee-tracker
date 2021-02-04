8ac06d7faa6f87a1de90e2a001f35065bcaa727b
Jose <jose42cordova@gmail.com>
2021-02-04 16:12:52 -0600

    improved schema and seeds

--- schema.sql
+++ schema.sql
@@ -2,20 +2,23 @@ use work_place_db;
 
 create table department (
 	id int not null auto_increment primary key,
-    name varchar(30)
+    name varchar(30) not null
 );
 
 create table role (
 	id int not null auto_increment primary key,
-    title varchar(30),
-    salary decimal,
-    department_id int
+    title varchar(30) not null,
+    salary decimal not null,
+    department_id int not null,
+    foreign key (department_id) references department(id)
 );
 
 create table employee (
 	id int not null auto_increment primary key,
-    first_name varchar(30),
-    last_name varchar(30),
-    role_id int,
-    manager_id int
+    first_name varchar(30) not null,
+    last_name varchar(30) not null,
+    role_id int not null,
+    manager_id int null,
+    foreign key (role_id) references role(id),
+    foreign key (manager_id) references employee(id)
 );
--- seeds.sql
+++ seeds.sql
@@ -4,11 +4,11 @@ insert into department (name)
 values ("Legal"), ("Accounting"), ("HR"), ("Sales");
 
 insert into role (title, salary, department_id)
-values ("Lawyer", 120000, 100), ("Paralegal", 50000, 100), ("Accountant", 70000, 200), 
-("HR Manager", 80000, 300), ("Secretary", 40000, 300), ("Sales Lead", 70000, 400), 
-("Salesman", 50000, 400);
+values ("Lawyer", 120000, 1), ("Paralegal", 50000, 1), ("Accountant", 70000, 2), 
+("HR Manager", 80000, 3), ("Secretary", 40000, 3), ("Sales Lead", 70000, 4), 
+("Salesman", 50000, 4);
 
 insert into employee (first_name, last_name, role_id, manager_id)
-values ("Saul", "Goodman", 101, null), ("Mike", "Ehrmantraut", 102, 111), ("Skyler", "White", 201, null), 
-("Gus", "Fring", 301, null), ("Jane", "Margolis", 302, 311), ("Walter", "White", 401, null), 
-("Jesse", "Pinkman", 402, 411), ("Brandon", "Mayhew", 402, 411), ("Skinny", "Pete", 402, 411);
+values ("Saul", "Goodman", 1, null), ("Mike", "Ehrmantraut", 2, 1), ("Skyler", "White", 3, null), 
+("Gus", "Fring", 4, null), ("Jane", "Margolis", 5, 4), ("Walter", "White", 6, null), 
+("Jesse", "Pinkman", 7, 6), ("Brandon", "Mayhew", 7, 6), ("Skinny", "Pete", 7, 6);
